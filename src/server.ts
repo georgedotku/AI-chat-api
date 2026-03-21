b30b694442de65e205afe03c546f72ca21c64a30
George Octavius <113178814+georgetheoracle@users.noreply.github.com>
2026-03-21 13:38:37 -0400

    update

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { StreamChat } from 'stream-chat';
 
+
 dotenv.config();
 const app = express();
 
@@ -11,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Initialize StreamChat client
-const streamClient = StreamChat.getInstance(process.env.STREAM_API_KEY!, process.env.STREAM_API_SECRET!);
+const chatClient = StreamChat.getInstance(process.env.STREAM_API_KEY!, process.env.STREAM_API_SECRET!);
 // Register user with StreamChat
 app.post('/register', async (req: Request, res: Response): Promise<any> => {
     const { name, email } = req.body;
@@ -21,14 +22,25 @@ app.post('/register', async (req: Request, res: Response): Promise<any> => {
     try {
         const userId = email.replace(/[^a-zA-Z0-9_-]/g, '_');
         console.log(userId);
-        // Check if user already exists
-        const existingUser = await streamClient.queryUsers({ id: userId });
-        console.log(existingUser);
+        // Check if user exists
+        const userResponse = await chatClient.queryUsers({ id: { $eq: userId } });
+
+        if (!userResponse.users.length) {
+            // Add new user to stream
+            await chatClient.upsertUser({
+                id: userId,
+                name,
+                email,
+                role: 'user',
+            });
+        }
         res.status(200).json({ message: 'Success' })
     } catch (error) {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
